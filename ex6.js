55bcba83a2c684329d3478a1da6740f62e316a17
20087985 <20087985@mydbs.ie>
2026-05-06 22:45:15 +0100

    Exercise 6

--- ex6.js
+++ ex6.js
@@ -1,4 +1,30 @@
-//This should have a function that takes a GitHub username 
-//and outputs their repo with the most forks.
+let getMostForkedRepo = (username) => {
+    
+    const url = ⁠ "https://api.github.com/users/${username}/repos" ⁠;
 
-//https://api.github.com/
+  
+    fetch(url)
+        .then(response => {
+            
+            if (!response.ok) throw new Error("User not found");
+            return response.json();
+        })
+        .then(repos => {
+            
+            if (repos.length === 0) {
+                alert("This user has no repositories.");
+                return;
+            }
+
+           
+            repos.sort((a, b) => b.forks_count - a.forks_count);
+
+            let topRepo = repos[0];
+
+           
+            let message = ⁠ Top Repo: ${topRepo.name} with ${topRepo.forks_count} forks. ⁠;
+            
+            alert(message);
+        });
+};
+
