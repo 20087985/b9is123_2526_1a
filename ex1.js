bb9fde57893bbf7f229a51400076e48d6d84064f
20087985 <20087985@mydbs.ie>
2026-05-06 23:55:44 +0100

    fixed issues in the exercise 1 code

--- ex1.js
+++ ex1.js
@@ -1,32 +1,28 @@
-//write a function to alert if overweight (15kg) baggage
+let check = () => {
+    let weight = parseInt(document.getElementById('weight').value);
+ 
+    if (weight > 15) {
+        alert("Baggage is overweight! (Limit: 15kg)");
+        document.getElementById('removed').style.display = 'block';
+        document.getElementById('remB').style.display = 'block';
+    } else {
+        alert("Weight is okay. You may proceed.");
+        
+        document.getElementById('removed').style.display = 'none';
+        document.getElementById('remB').style.display = 'none';
+    }
+}
 
-//call it from the page
-
-            //alert("Hello");//write a check function here...
-            //update: the check function should reveal the "removed" text input
-            //if the baggage is overweight
-            //and a button to remove some
-            //the remove function should update the weight
-            //and let the user know when ok to proceed
-            let check=()=>{
-              let weight = parseInt(document.getElementById('weight').value);
-              alert('Checking Baggage weight: '+parseInt(document.getElementById('weight').value));
-              //These should only display if it is actually overweight
-              if(weight > 15)
-              {
-                          
-              document.getElementById('removed').style='display:block;'
-              document.getElementById('remB').style='display:block;'
-              }
-            }
-            let remove=()=>{
-              
-              let currentWeight = parseInt(document.getElementById('weight').value);
-              let weightToRemove = parseInt(document.getElementById('removed').value); 
-              let newWeight = currentWeight - weightToRemove;
-              alert('Removing weight: '+parseInt(document.getElementById('removed').value)); 
-              check();
-            
-            }
-
-            //Test edit no. 2;
+let remove = () => {
+    let currentWeight = parseInt(document.getElementById('weight').value);
+    let weightToRemove = parseInt(document.getElementById('removed').value); 
+    
+    
+    let newWeight = currentWeight - weightToRemove;
+    document.getElementById('weight').value = newWeight; 
+    
+    alert('Removed ' + weightToRemove + 'kg. New weight is ' + newWeight + 'kg.'); 
+    
+  
+    check();
+}
