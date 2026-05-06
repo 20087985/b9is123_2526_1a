4689a074f00de9c18eacfd23151f6ed77dc8208d
20087985 <20087985@mydbs.ie>
2026-05-06 23:58:17 +0100

    updated the remove method to update the checkin weight after removing the excess

--- ex1.js
+++ ex1.js
@@ -13,16 +13,20 @@ let check = () => {
     }
 }
 
+
 let remove = () => {
+    // 1. Get the numbers from the page
     let currentWeight = parseInt(document.getElementById('weight').value);
     let weightToRemove = parseInt(document.getElementById('removed').value); 
     
-    
+    // 2. Do the math
     let newWeight = currentWeight - weightToRemove;
+    
+    // 3. THIS IS THE MISSING PART: Update the box on the screen!
     document.getElementById('weight').value = newWeight; 
     
-    alert('Removed ' + weightToRemove + 'kg. New weight is ' + newWeight + 'kg.'); 
+    alert('Removing weight: ' + weightToRemove + 'kg'); 
     
-  
+    // 4. Now when check() runs, it looks at the box and sees the NEW number
     check();
 }
