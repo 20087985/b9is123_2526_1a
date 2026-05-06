6d00e5a0e5aba2554ce5b2d7217be4df87fae0b7
20087985 <20087985@mydbs.ie>
2026-05-06 22:41:05 +0100

    Excercise 5

--- ex5.js
+++ ex5.js
@@ -1,27 +1,61 @@
-    let parse=(i)=>{
-      o=parseInt(i)
-      if (isNaN(o))
-        throw("NaN");
-      return o;
+
+let parse = (i) => {
+    let o = parseInt(i);
+    if (isNaN(o)) {
+        throw ("NaN");
+    }
+    return o;
+}
+
+let processEulerCorrupt = (inputString) => {
+  
+        let parts = inputString.split(":");
+        
+        
+        if (parts.length < 2) {
+            throw "MissingColon";
+        }
+
+       
+        let factors = parts[0].trim().split(" ").map(f => parse(f));
+        let multiples = parts[1].trim().split(" ").map(m => parse(m));
+
+        
+        let sum = 0;
+        for (let num of multiples) {
+            if (factors.some(f => num % f === 0)) {
+                sum += num;
+            }
+        }
+        return sum + " : " + parts[0].trim() + " : " + parts[1].trim();
+
+   
+}
+
+
+let processEulerOmitCorrupt = (inputString) => {
+    let parts = inputString.split(":");
+    if (parts.length < 2) return "corrupt : " + inputString;
+
+   
+    let cleanList = (str) => {
+        return str.trim().split(" ").filter(item => {
+          
+                parse(item);
+                return true; 
+           
+        }).map(Number);
+    };
+
+    let factors = cleanList(parts[0]);
+    let multiples = cleanList(parts[1]);
+
+    let sum = 0;
+    for (let num of multiples) {
+        if (factors.some(f => num % f === 0)) {
+            sum += num;
+        }
     }
     
-//Create a modified version of ex4 that prepends "corrupt" where an error occurs
-
-//Examples ()
-// 2 3 5 67
-// corrupt : 2 3 5 67
-
-//Actually this one will not result in an error unless you throw one as in parse(i) above:
-// 3 5 hello: 1 2 3 4 5 6 7 8 9
-// corrupt : 3 5 hello: 1 2 3 4 5 6 7 8 9
-
-//Create a further version that will output the answer omitting corrupted values
-//or "corrupt" if it cannot be resolved
-
-// 3 5 hello: 1 2 3 4 5 6 7 8 9
-// 23 : 3 5 hello: 1 2 3 4 5 6 7 8 9
-
-//this cannot be fixed - actually this is the only one that will throw errors
-// 2 3 5 67
-// corrupt : 2 3 5 67
-
+    return sum + " : " + parts[0].trim() + " : " + parts[1].trim();
+}
