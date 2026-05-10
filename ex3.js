16a5d18f42cebe783a512281d79bafdf6dfa6889
20087985 <20087985@mydbs.ie>
2026-05-10 16:11:53 +0100

    fixing the closing parentheis issue. on eloop was not closed.

--- ex3.js
+++ ex3.js
@@ -1,57 +1,52 @@
-let listEuler1=(a,b,l)=>{
-    let sum =0;
+let listEuler1 = (a, b, l) => {
+    let sum = 0;
     for (let num of l) {
-        if(num % a === 0 || num % b === 0){
+        if (num % a === 0 || num % b === 0) {
             sum = sum + num;
         }
-        
     }
     return sum;
-    
 }
 
-let listEuler2=(a,l)=>{
+let listEuler2 = (a, l) => {
     let sum = 0;
-    for (let num of l){
-        if (a.some(divisor => num % divisor === 0)
-        {
+    for (let num of l) {
+        // Added the missing closing parenthesis here )
+        if (a.some(divisor => num % divisor === 0)) {
             sum = sum + num;
         }
-        
     }
     return sum;
 }
-let listEuler3=(a,l)=>{
-    return l.reduce((total, num) =>{
+
+let listEuler3 = (a, l) => {
+    return l.reduce((total, num) => {
         let isMultiple = false;
-        for (let divisor of a){
-            if(num % divisor === 0) isMultiple = true;
-            
+        for (let divisor of a) {
+            if (num % divisor === 0) isMultiple = true;
         }
         return isMultiple ? total + num : total;
     }, 0);
 }
 
-
-let eulerlist=()=>
-{
-    a=2//can un-hardcode
-    b=3
-    l=[1,2,3,4,5,6,7,9,10,10,10]
-    let result = listEuler1(a,b,l);
-    alert("The sum is:" + result);
-   
-}
-let euler2Lists=()=>
-{
-    a=[2,3]//can un-hardcode
-    l=[1,2,3,4,5,6,7,9,10,10,10]
-    //call listEuler2 and alert.
+let eulerlist = () => {
+    let a = 2;
+    let b = 3;
+    let l = [1, 2, 3, 4, 5, 6, 7, 9, 10, 10, 10];
+    let result = listEuler1(a, b, l);
+    alert("ListEuler1 Sum: " + result);
 }
 
-let euler2Lists1=()=>
-{
-    a=[2,3,5]//can un-hardcode
-    l=[1,2,3,4,5,6,7,9,10,10,10]
-    //call listEuler3 and alert.
+let euler2Lists = () => {
+    let a = [2, 3];
+    let l = [1, 2, 3, 4, 5, 6, 7, 9, 10, 10, 10];
+    let result = listEuler2(a, l); // Now calling the function
+    alert("ListEuler2 Sum: " + result);
+}
+
+let euler2Lists1 = () => {
+    let a = [2, 3, 5];
+    let l = [1, 2, 3, 4, 5, 6, 7, 9, 10, 10, 10];
+    let result = listEuler3(a, l); // Now calling the function
+    alert("ListEuler3 Sum: " + result);
 }
