1d5604c09ccc275c94c619a90ec67a5ac3984cb8
20087985 <20087985@mydbs.ie>
2026-05-10 16:45:55 +0100

    Chnaged the hardcoded input values to take the input from html directly

--- ex3.js
+++ ex3.js
@@ -1,53 +1,62 @@
+
+
 let listEuler1 = (a, b, l) => {
     let sum = 0;
     for (let num of l) {
         if (num % a === 0 || num % b === 0) {
-            sum = sum + num;
+            sum += num;
         }
     }
     return sum;
 }
 
 let listEuler2 = (a, l) => {
-  
-    let filteredList = l.filter(num => a.some(divisor => num % divisor === 0));
-    
-   
     let sum = 0;
-    for (let num of filteredList) {
-        sum += num;
+    for (let num of l) {
+        if (a.some(divisor => num % divisor === 0)) {
+            sum += num;
+        }
     }
     return sum;
 }
 
+
 let listEuler3 = (a, l) => {
     return l.reduce((total, num) => {
-        let isMultiple = false;
-        for (let divisor of a) {
-            if (num % divisor === 0) isMultiple = true;
-        }
+        let isMultiple = a.some(divisor => num % divisor === 0);
         return isMultiple ? total + num : total;
     }, 0);
 }
 
+
+
 let eulerlist = () => {
-    let a = 2;
-    let b = 3;
-    let l = [1, 2, 3, 4, 5, 6, 7, 9, 10, 10, 10];
-    let result = listEuler1(a, b, l);
-    alert("ListEuler1 Sum: " + result);
+
+    let a = parseInt(document.getElementById('a1').value);
+    let b = parseInt(document.getElementById('b1').value);
+    
+
+    let lString = document.getElementById('l').value;
+    let lArray = lString.split(" ").map(Number); 
+
+    let result = listEuler1(a, b, lArray);
+    alert("Sum of multiples of " + a + " or " + b + " is: " + result);
 }
 
 let euler2Lists = () => {
-    let a = [2, 3];
-    let l = [1, 2, 3, 4, 5, 6, 7, 9, 10, 10, 10];
-    let result = listEuler2(a, l); // Now calling the function
-    alert("ListEuler2 Sum: " + result);
+
+    let aArray = document.getElementById('aList').value.split(" ").map(Number);
+    let lArray = document.getElementById('mList').value.split(" ").map(Number);
+
+    let result = listEuler2(aArray, lArray);
+    alert("Sum (List A length 2) result: " + result);
 }
 
 let euler2Lists1 = () => {
-    let a = [2, 3, 5];
-    let l = [1, 2, 3, 4, 5, 6, 7, 9, 10, 10, 10];
-    let result = listEuler3(a, l); // Now calling the function
-    alert("ListEuler3 Sum: " + result);
+
+    let aArray = document.getElementById('aList').value.split(" ").map(Number);
+    let lArray = document.getElementById('mList').value.split(" ").map(Number);
+
+    let result = listEuler3(aArray, lArray);
+    alert("Sum (Any length List A) result: " + result);
 }
