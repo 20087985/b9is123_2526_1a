c2d20262be42d2538f81ddae059a5502fa2abaf0
20087985 <20087985@mydbs.ie>
2026-05-10 17:06:14 +0100

    Fixed the typos

--- ex4.js
+++ ex4.js
@@ -1,7 +1,8 @@
+
 let getSumOfMultiples = (factors, multiples) => {
-  let sum =0;
-  for (let num of multiples){
-    if (factors.some(f => num % f === 0)){
+  let sum = 0;
+  for (let num of multiples) {
+    if (factors.some(f => num % f === 0)) {
       sum = sum + num;
     }
   }
@@ -9,11 +10,18 @@ let getSumOfMultiples = (factors, multiples) => {
 };
 
 let processEulerString = (inputString) => {
+
   let parts = inputString.split(":");
-  let factors = parts[0].trim( ).split(" ").map(Numbeer);
-  let multiples = parts[1].trim( ).split(" ").map(Numbeer);
-  let resultSum = getSumOfMultiples(factors,multiples);
+  
+
+  let factors = parts[0].trim().split(" ").map(Number);
+  let multiples = parts[1].trim().split(" ").map(Number);
+
+  let resultSum = getSumOfMultiples(factors, multiples);
+  
+
   let output = resultSum + " : " + parts[0].trim() + " : " + parts[1].trim();
+  
   alert(output);
-  return output
+  return output;
 };
